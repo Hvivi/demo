d199cb00ee27445aedba09b7e49c53e71563e882
Hvivi <744935958@qq.com>
2016-07-05 19:25:37 +0800

    demo

---
+++ practice/library.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Library-practice</title>
+		<link rel="stylesheet" href="libraryTwo.css" />
+	</head>
+	<body>
+		<div class="container">
+			<div id="top"class="row">
+	            <div id="links"><!--... --></div>
+			    <div id="logo"><a href="#"><img src="img/logo.gif" width="750px"/></a></div>
+            </div> 
+			<nav>
+			<?php
+			   include("menu02.php");
+			?>  
+			</nav>
+			<!--<div class="row main">
+					<div  class="clearFix"id="left">
+						<div>近期热点<br />
+						  <img src="img/003.jpg" />
+						  <span>8月25日，国家图书馆民政部分馆成立暨国家图书馆立法决策服务民政部平台
+							开通仪式在民政部隆重举行。
+							相比以往国家图书馆部委分馆建设，民政部与国家图书馆......</span>
+						</div>
+						<div>近期热点<br />
+						  <img src="img/004.jpg" />
+							<div>
+								<ul>
+									<li><a href="">美国三机构共同宣布首个国际开放存取日确立</a></li>
+									<li><a href="">世界了解中国、中国走向世界的重要平台第十</a></li>
+									<li><a href="">上图馆藏历史文献接轨数字化时代</a></li>
+									<li><a href="">南京图书馆为残障人群营造无障碍空间</a></li>
+								</ul>
+						    </div>
+						</div>
+					</div>
+					<div id="right">国图要闻</br>
+						<div>
+					    	<ul>
+					    		<li><a href="">文化部副部长周和平参加国家图书馆民政部分馆
+					    			成立暨国家图书馆立法决策服务民政部平台开通仪式（附图）</a></li>
+					    		<li><a href="">我馆代表团参加第七十四届国际图联大会（附图）</a></li>
+					    		<li><a href="">国家图书馆民政部分馆成立暨国家图书馆立法决策服务
+					    			民政部平台开通仪式在书刊检索</a></li>
+					    	</ul>
+					    </div>
+					</div>
+			</div>-->
+			<div id="main">
+				<div id="contend">
+					<div id="ontend_left">
+						<div id="news">
+						    <div class="biaoti"><span>近期热点</span></div>
+							<div class="rediancontent">
+							  <div class="rediancontentimg"><a href="#"><img src="img/003.jpg" width="91" height="70" /></a></div>
+							  <div class="rediancontentp"><a href="#"><p>8月25日，国家图书馆民政部分馆成立暨国家图书馆立法决策服务民政部平台开通仪式在民政部隆重举行。相比以往国家图书馆部委分馆建设，民政部与国家图书馆......</p></a>
+							  
+							    <p style="text-align:right"><a href><span class="more">更多>></span></a></p>
+							  </div>
+							</div>
+							 <div class="redihr" style="margin-top:10px; margin-bottom:5px; border-top:1px solid #D8D8D8; width:340px;"></div>
+							 <div class="biaoti"><span>近期热点</span></div>
+							 <div class="rediancontent">
+							  <div class="rediancontentimg"><a href="#"><img src="img/004.jpg" width="91" height="70" /></a></div>
+							  <div class="rediancontentp">
+							 
+							    <ul style="list-style:square; padding-left:15px; margin:0; padding-top:0; line-height:15px;">
+								   <li><a href="#">美国三机构共同宣布首个国际开放存取日确立</a></li>
+								   <li><a href="#">世界了解中国、中国走向世界的重要平台第十</a></li>
+								   <li><a href="#">上图馆藏历史文献接轨数字化时代</a></li>
+								   <li><a href="#">南京图书馆为残障人群营造无障碍空间</a></li>
+								   <li style="text-align:right; list-style:none"><a href="#"><span class="more">更多>></span></a></li>
+								</ul>
+							  </div>
+							</div>
+						 </div>
+	   				</div>
+
+						
+					</div>
+					<div id="contend_right"></div>
+				</div>
+				<div id="information"></div>
+			</div>
+			<footer>脚部</footer>
+		</div>
+	</body>
+</html>
---
+++ practice/page02.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Library-practice</title>
+		<link rel="stylesheet" href="library.css" />
+	</head>
+	<body>
+		<header class="center">
+			<img src="img/logo.gif"
+		</header>
+		<nav>
+			<?php
+			   include("menu02.php");
+			?>   
+		</nav>
+		<div class="row">
+			<div>
+				<div>
+					近期热点<br />
+					<img src="img/003.jpg" />
+					<p>8月25日，国家图书馆民政部分馆成立暨国家图书馆立法决策服务民政部平台
+						开通仪式在民政部隆重举行。
+						相比以往国家图书馆部委分馆建设，民政部与国家图书馆......</p>
+				</div>
+				<div>
+					近期热点<br />
+					<img src="img/004.jpg" />
+					<div>
+						<ul>
+							<li><a href="">美国三机构共同宣布首个国际开放存取日确立</a></li>
+							<li><a href="">世界了解中国、中国走向世界的重要平台第十</a></li>
+							<li><a href="">上图馆藏历史文献接轨数字化时代</a></li>
+							<li><a href="">南京图书馆为残障人群营造无障碍空间</a></li>
+						</ul>
+				    </div>
+			</div>
+			<div>
+			    国图要闻<br/>
+			    <div>
+			    	<ul>
+			    		<li><a href="">文化部副部长周和平参加国家图书馆民政部分馆
+			    			成立暨国家图书馆立法决策服务民政部平台开通仪式（附图）</a></li>
+			    		<li><a href="">我馆代表团参加第七十四届国际图联大会（附图）</a></li>
+			    		<li><a href="">国家图书馆民政部分馆成立暨国家图书馆立法决策服务
+			    			民政部平台开通仪式在书刊检索</a></li>
+			    	</ul>
+			    </div>
+			    <div>
+			    	书刊检索
+			    </div>
+			</div>
+		</div>
+	<div class="footer clearFix row">
+	    <div>
+	    	<img src="img/Copy of 005.jpg"  height="168px"/>
+	    </div> 
+	    <div>
+	    	<ul>
+	    		<li><a href="">图书</a></li>
+	    		<li><a href="">期刊</a></li>
+	    		<li><a href="">报纸</a></li>
+	    		<li><a href="">学位论文</a></li>
+	    		<li><a href="">善本古籍文献</a></li>
+	    		<li><a href="">特藏专藏</a></li>
+	    		<li><a href="">缩微资料</a></li>
+	    	</ul>
+	    </div>
+	    <div>
+	    	<ul>
+	    		<li><a href="">信息咨询</a></li>
+	    		<li><a href="">查新查证</a></li>
+	    		<li><a href="">电子信息服务</a></li>
+	    		<li><a href="">文献提供</a></li>
+	    		<li><a href="">讲座培训</a></li>
+	    		<li><a href="">基层图书馆服务</a></li>
+	    		<li><a href="">中文资料服务中心</a></li>
+	    	</ul>
+	    </div>
+	</div>
+	</div>
+	<footer>
+		脚部
+	</footer>
+    </body>
+</html>    
+
